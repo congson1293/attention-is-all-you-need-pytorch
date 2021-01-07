b8c1372b4c367e6c331f602dd937b199667fcb9e
sontc <congson1293@gmail.com>
2021-01-07 17:43:35 +0700

    resotre

--- train.py
+++ train.py
@@ -58,10 +58,9 @@ def cal_loss(pred, gold, trg_pad_idx, smoothing=False):
 
         non_pad_mask = gold.ne(trg_pad_idx)
         loss = -(one_hot * log_prb).sum(dim=1)
-        loss = loss.masked_select(non_pad_mask).sum() / non_pad_mask.sum()
+        loss = loss.masked_select(non_pad_mask).sum()  # average later
     else:
-        log_prb = F.log_softmax(pred, dim=1)
-        loss = F.cross_entropy(log_prb, gold, ignore_index=trg_pad_idx, reduction='mean')
+        loss = F.cross_entropy(pred, gold, ignore_index=trg_pad_idx, reduction='sum')
     return loss
 
 
